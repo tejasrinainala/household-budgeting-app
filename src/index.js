aab2ceddc36f61e209f3174d1d9321b37377d8ad
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2025-05-25 19:59:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App'; // Adjust the path based on your file structure
-import './index.css'; // Ensure the CSS path is correct
+import App from './components/App'; 
+import './index.css'; 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
